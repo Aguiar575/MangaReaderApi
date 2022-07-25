ed72b922a0f916fb6f951aa34c4e004e3c38e592
Aguiar575 <aguiarus99arthurus@gmail.com>
2022-07-24 21:22:01 -0300

    refactoring ServiceSourceResolverTests

--- MangaReaderApi/MangaReaderApi.Tests/Domain/ServiceSourceResolverTests.cs
+++ MangaReaderApi/MangaReaderApi.Tests/Domain/ServiceSourceResolverTests.cs
@@ -10,21 +10,16 @@ namespace MangaReaderApi.Tests.Domain;
 
 public class ServiceSourceResolverTests
 {
-    IList<MangaSource> MangaSource = new List<MangaSource>
+    [Fact]
+    public void ResolveSource_Should_Return_One_Source()
+    {
+        IList<MangaSource> MangaSource = new List<MangaSource>
         { new MangaSource("SourceName", "//div/img") };
 
-    private IServiceSourceResolver sut;
-
-    public ServiceSourceResolverTests()
-    {
         var jsonReader = new Mock<IServiceJasonReader>();
         jsonReader.Setup(sr => sr.LoadJson(It.IsAny<string>())).Returns(() => MangaSource);
-        sut = new ServiceSourceResolver(jsonReader.Object);
-    }
+        var sut = new ServiceSourceResolver(jsonReader.Object);
 
-    [Fact]
-    public void ShouldReturnOneSource()
-    {
         var selectedSource = sut.ResolveSource("SourceName");
 
         Assert.Equal("SourceName", selectedSource.SourceName);
@@ -32,8 +27,12 @@ public class ServiceSourceResolverTests
     }
 
     [Fact]
-    public void ShouldReturnSourceNootFoundException()
+    public void ResolveSource_Should_Return_Source_Not_Found_Exception()
     {
+        var jsonReader = new Mock<IServiceJasonReader>();
+        jsonReader.Setup(sr => sr.LoadJson(It.IsAny<string>())).Returns(() => new List<MangaSource>());
+        var sut = new ServiceSourceResolver(jsonReader.Object);
+
         Assert.Throws<SourceNotFoundException>(() => sut.ResolveSource(""));
     }
 }
